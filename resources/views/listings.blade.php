2486e7ac14892c923db7aa7bab2c7181c7ccda2e
Ranto Bastara Hamonangan Sitorus <rantositorus44@gmail.com>
2024-10-16 09:38:19 +0700

    feat: filtered listings page only show 'available'

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -103,108 +103,112 @@
             <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4 py-2">
 
                 @foreach ($listings as $listing)
-                    @if ($listing->is_in_wishlist)
-                        @php $is_wishlist = true; @endphp
-                    @endif
-                    <div
-                        class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
-                        <div class="h-56 w-full">
-                            <a href="/listings/{{ $listing->slug }}">
-                                <img class="mx-auto h-full dark:hidden"
-                                    src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg"
-                                    alt="" />
-                                <img class="mx-auto hidden h-full dark:block"
-                                    src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg"
-                                    alt="" />
-                            </a>
-                        </div>
-                        <div class="pt-6">
-                            <div class="mb-4 flex items-center justify-between gap-4">
-                                <span
-                                    class="me-2 rounded {{ $listing->category->color }} px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">{{ $listing->category->name }}</span>
+                    @if ($listing->status == 'Available')
+                        @if ($listing->is_in_wishlist)
+                            @php $is_wishlist = true; @endphp
+                        @endif
+                        <div
+                            class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+                            <div class="h-56 w-full">
+                                <a href="/listings/{{ $listing->slug }}">
+                                    <img class="mx-auto h-full dark:hidden"
+                                        src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg"
+                                        alt="" />
+                                    <img class="mx-auto hidden h-full dark:block"
+                                        src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg"
+                                        alt="" />
+                                </a>
+                            </div>
+                            <div class="pt-6">
+                                <div class="mb-4 flex items-center justify-between gap-4">
+                                    <span
+                                        class="me-2 rounded {{ $listing->category->color }} px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">{{ $listing->category->name }}</span>
 
-                                <div class="flex items-center justify-end gap-1">
-                                    @if ($listing->is_in_wishlist)
-                                        <form action="{{ route('wishlist.remove', $listing->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit"
-                                                data-tooltip-target="tooltip-remove-from-favorites-{{ $listing->id }}"
-                                                class="rounded-lg p-2 text-red-500 hover:bg-gray-100 hover:text-gray-900 dark:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                                                <span class="sr-only">Remove From Wishlist</span>
-                                                <svg class="h-5 w-5 text-red-500" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24">
-                                                    <path stroke="currentColor" stroke-linecap="round"
-                                                        stroke-linejoin="round" stroke-width="2"
-                                                        d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
-                                                </svg>
-                                            </button>
-                                            <div id="tooltip-remove-from-favorites-{{ $listing->id }}" role="tooltip"
-                                                class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-                                                data-popper-placement="top">
-                                                Remove From Wishlist
-                                                <div class="tooltip-arrow" data-popper-arrow=""></div>
-                                            </div>
-                                        </form>
-                                    @else
-                                        <form action="/wishlist/{{ $listing->id }}/add" method="POST"
-                                            class="inline">
-                                            @csrf
-                                            <button type="submit"
-                                                data-tooltip-target="tooltip-add-to-favorites-{{ $listing->id }}"
-                                                class="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                                @if ($listing->storeowner->user_id == auth()->id()) disabled @endif>
-                                                <span
-                                                    class="sr-only">{{ $listing->is_in_wishlist ? 'Remove From Wishlist' : 'Add to Wishlist' }}</span>
-                                                <svg class="h-5 w-5 {{ $listing->is_in_wishlist ? 'text-red-500' : 'text-gray-500' }}"
-                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none" viewBox="0 0 24 24">
-                                                    <path stroke="currentColor" stroke-linecap="round"
-                                                        stroke-linejoin="round" stroke-width="2"
-                                                        d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
-                                                </svg>
-                                            </button>
-                                            <div id="tooltip-add-to-favorites-{{ $listing->id }}" role="tooltip"
-                                                class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
-                                                data-popper-placement="top">
-                                                {{ $listing->is_in_wishlist ? 'Remove From Wishlist' : 'Add to Wishlist' }}
-                                                <div class="tooltip-arrow" data-popper-arrow=""></div>
-                                            </div>
-                                        </form>
-                                    @endif
+                                    <div class="flex items-center justify-end gap-1">
+                                        @if ($listing->is_in_wishlist)
+                                            <form action="{{ route('wishlist.remove', $listing->id) }}"
+                                                method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit"
+                                                    data-tooltip-target="tooltip-remove-from-favorites-{{ $listing->id }}"
+                                                    class="rounded-lg p-2 text-red-500 hover:bg-gray-100 hover:text-gray-900 dark:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white">
+                                                    <span class="sr-only">Remove From Wishlist</span>
+                                                    <svg class="h-5 w-5 text-red-500" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24">
+                                                        <path stroke="currentColor" stroke-linecap="round"
+                                                            stroke-linejoin="round" stroke-width="2"
+                                                            d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
+                                                    </svg>
+                                                </button>
+                                                <div id="tooltip-remove-from-favorites-{{ $listing->id }}"
+                                                    role="tooltip"
+                                                    class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                                                    data-popper-placement="top">
+                                                    Remove From Wishlist
+                                                    <div class="tooltip-arrow" data-popper-arrow=""></div>
+                                                </div>
+                                            </form>
+                                        @else
+                                            <form action="/wishlist/{{ $listing->id }}/add" method="POST"
+                                                class="inline">
+                                                @csrf
+                                                <button type="submit"
+                                                    data-tooltip-target="tooltip-add-to-favorites-{{ $listing->id }}"
+                                                    class="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                                    @if ($listing->storeowner->user_id == auth()->id()) disabled @endif>
+                                                    <span
+                                                        class="sr-only">{{ $listing->is_in_wishlist ? 'Remove From Wishlist' : 'Add to Wishlist' }}</span>
+                                                    <svg class="h-5 w-5 {{ $listing->is_in_wishlist ? 'text-red-500' : 'text-gray-500' }}"
+                                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none" viewBox="0 0 24 24">
+                                                        <path stroke="currentColor" stroke-linecap="round"
+                                                            stroke-linejoin="round" stroke-width="2"
+                                                            d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
+                                                    </svg>
+                                                </button>
+                                                <div id="tooltip-add-to-favorites-{{ $listing->id }}" role="tooltip"
+                                                    class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                                                    data-popper-placement="top">
+                                                    {{ $listing->is_in_wishlist ? 'Remove From Wishlist' : 'Add to Wishlist' }}
+                                                    <div class="tooltip-arrow" data-popper-arrow=""></div>
+                                                </div>
+                                            </form>
+                                        @endif
+                                    </div>
+
+
                                 </div>
 
+                                <a href="/listings/{{ $listing->slug }}"
+                                    class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{{ Str::limit($listing->name, 64) }}</a>
+                                <br>
+                                <a href="/store/{{ $listing->storeowner->store_slug }}"
+                                    class="hover:underline">{{ $listing->storeowner->store_name }}</a>
 
-                            </div>
+                                <div class="mt-4 flex items-center justify-between gap-4">
+                                    <p class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">
+                                        ${{ $listing->price }}</p>
 
-                            <a href="/listings/{{ $listing->slug }}"
-                                class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{{ Str::limit($listing->name, 64) }}</a>
-                            <br>
-                            <a href="/store/{{ $listing->storeowner->store_slug }}"
-                                class="hover:underline">{{ $listing->storeowner->store_name }}</a>
-
-                            <div class="mt-4 flex items-center justify-between gap-4">
-                                <p class="text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">
-                                    ${{ $listing->price }}</p>
-
-                                <form action="/cart/{{ $listing->id }}/add" method="POST" class="inline">
-                                    @csrf
-                                    <button type="submit" @if ($listing->storeowner->user_id == auth()->id()) disabled @endif
-                                        class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                        <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round"
-                                                stroke-linejoin="round" stroke-width="2"
-                                                d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-                                        </svg>
-                                        Add to cart
-                                    </button>
-                                </form>
+                                    <form action="/cart/{{ $listing->id }}/add" method="POST" class="inline">
+                                        @csrf
+                                        <button type="submit" @if ($listing->storeowner->user_id == auth()->id()) disabled @endif
+                                            class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                            <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
+                                            </svg>
+                                            Add to cart
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
             <!-- Filter modal -->
